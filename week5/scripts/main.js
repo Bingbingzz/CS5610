301369a73dcf6240a39333f532d512665ac2b503
Lydia <zhao.ce1@northeastern.edu>
2022-10-14 17:11:37 -0700

    radius and area calculation

---
+++ week5/scripts/main.js
@@ -0,0 +1,20 @@
+function getNumber(){
+    let num;
+    do{
+        num = prompt("Enter a number");
+    }while(isNaN(num));
+    return num;
+}
+
+let number = getNumber();
+let radius = document.querySelector("#radius")
+radius.innerText += ` ${number}`;
+
+function calculateArea(rad){
+    let area = Math.PI * Math.pow(rad,2);
+    return area;
+}
+
+let area = calculateArea(number);
+let result = document.querySelector("#result");
+result.innerText += ` ${area}`;
