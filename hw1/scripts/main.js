0b318f86949fa62a7d47deb153e8fd1ebf485258
Lydia <zhao.ce1@northeastern.edu>
2022-10-16 16:49:05 -0700

    Theme can be switched after pressing the button

--- hw1/scripts/main.js
+++ hw1/scripts/main.js
@@ -1,15 +1,41 @@
 
 
-window.onscroll = function() {myFunction()};
+window.onscroll = function() {stickNavBar()};
 
 var navbar = document.getElementById("navbar");
 var sticky = navbar.offsetTop;
 
-function myFunction() {
-  console.log("scroll");
+function stickNavBar() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
   } else {
     navbar.classList.remove("sticky");
   }
 }
+
+let buttonBG = document.querySelector("#buttonBG");
+buttonBG.innerText = localStorage.getItem("buttonText")?localStorage.getItem("buttonText"):`dark mode`;
+buttonBG.addEventListener("click", changeButtonText);
+function changeButtonText(){
+    if(buttonBG.innerText ==="dark mode"){
+        buttonBG.innerText = "light mode";
+        localStorage.setItem("buttonText", "light mode");
+    }else{
+         buttonBG.innerText = "dark mode";
+         localStorage.setItem("buttonText", "dark mode");
+    }
+}
+
+//Change to the theme last time user chose.
+mode = localStorage.getItem("buttonText");
+if (mode === "dark mode"){
+    changeBGStyle();
+}
+
+function changeBGStyle(){
+    var element = document.getElementById("myDiv");
+    element.classList.toggle("myStyle");   
+}
+
+
+
