957d69766c667aa5ad1499f5eeb8fb9067182f54
Lydia <zhao.ce1@northeastern.edu>
2022-10-16 18:58:41 -0700

    Date and time section added

--- hw1/scripts/main.js
+++ hw1/scripts/main.js
@@ -38,4 +38,23 @@ function changeBGStyle(){
 }
 
 
+const form = document.querySelector("form");
+const log = document.querySelector("#log");
 
+form.addEventListener("submit", (event) => {
+  const data = new FormData(form);
+  let output = "";
+  for (const entry of data) {
+    if(entry[0]==="date"){
+        date = new Date().toLocaleDateString();
+        entry[1] = date;
+    }
+    if(entry[0] ==="time"){
+        time = new Date().toLocaleTimeString();
+        entry[1] = time;
+    }
+    output = `\n${output}${entry[0]}: ${entry[1]}\n`; 
+  };
+  log.innerText = output;
+  event.preventDefault();
+}, false);
