371aa1368294770503e5e9bdfd27939ef06f9455
Lydia <zhao.ce1@northeastern.edu>
2022-11-07 23:44:26 -0800

    wait to add delete feature

--- hw2/app.js
+++ hw2/app.js
@@ -8,9 +8,9 @@ const mongoose = require("mongoose");
 
 
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const homeStartingContent = "While it is true that people with pets often experience greater health benefits than those without, a pet doesn’t necessarily have to be a dog or a cat. A rabbit could be ideal if you are allergic to other animals or have limited space but still want a furry friend to snuggle with. Birds can encourage social interaction and help keep your mind sharp if you are an older adult. Snakes, lizards, and other reptiles can make for exotic companions. Even watching fish in an aquarium can help reduce muscle tension and lower your pulse rate.acus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const aboutContent = "One of the reasons for these therapeutic effects is that pets fulfill the basic human need for touch. Even hardened criminals in prison show long-term changes in their behavior after interacting with pets, many of them experiencing mutual affection for the first time. Stroking, hugging, or otherwise touching a loving animal can rapidly calm and soothe you when you’re stressed or anxious. The companionship of a pet can also ease loneliness, and most dogs are a great stimulus for healthy exercise, which can substantially boost your mood and ease depression.";
+const contactContent = "Playing with a dog, cat, or other pet can elevate levels of serotonin and dopamine, which calm and relax.";
 
 const app = express();
 
@@ -61,8 +61,6 @@ app.get("/", function(req,res){
         });
     }
   });
-
-
 });
 
 app.get("/about", function(req, res) {
@@ -109,46 +107,17 @@ const requestedPostId = req.params.postId;
 });
 
 
-// app.post("posts/:postId",function(req, res){
-//   const deleteId = req.params.postId
-//
-//   Post.findOneAndRemove({deleteId},function(err, post){
-//     post.save(function(err){
-//       if (!err){
-//         res.redirect("/");
-//       }
-//     });
-//   });
-// });
 
-
-// app.get("/posts/:postName", function(req, res){
-//   const requestedTitle = _.lowerCase(req.params.postName);
-//   posts.forEach(function(post){
-//     const storedTitle = _.lowerCase(post.title);
-//     if (storedTitle === requestedTitle){
-//       res.render("post",{
-//         title: post.title,
-//         content: post.content
-//       });
-//     }
-//   })
-// })
-//
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.post("/delete",function(req, res){
+  const deleteId = req.params.postId
+  Post.findByIdAndDelete(deleteId, function(err, post){ 
+      if (!err){
+        res.redirect("/");
+      }else{
+        console.log(err);
+      }
+  });
+});
 
 
 app.listen(3000, function() {
